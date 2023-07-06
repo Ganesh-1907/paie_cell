c2bc0dcfa13e994cdf23449a05a885c29105afea
Ganesh <masterminds0809@gmail.com>
2023-07-07 01:03:04 +0530

    ok

--- paie_frontend/src/component/Home/Home.js
+++ paie_frontend/src/component/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "../header/header";
 function Home(){
     return(
         <>
         <div className="main">
-            <h1>PAIE CELL</h1>
+            <Header/>
         </div>
         
         </>
