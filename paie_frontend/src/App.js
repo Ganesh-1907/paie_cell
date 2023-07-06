01ac4a2aae6ce6a13656d144a24a7997b6f6a23f
Ganesh <masterminds0809@gmail.com>
2023-07-06 21:51:51 +0530

    ok

---
+++ paie_frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from "./Home";
+function App(){
+    return(
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/home" element={<Home/>}></Route>
+    </Routes>
+    </BrowserRouter>
+    </>
+    )
+}
+export default App
