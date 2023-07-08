e73aaf1f8c01e20bf303af8e7f7764004ef9db89
Ganesh <masterminds0809@gmail.com>
2023-07-08 16:20:04 +0530

    ok

--- paie_frontend/src/component/introduction/introduction.js
+++ paie_frontend/src/component/introduction/introduction.js
@@ -5,21 +5,83 @@ function Introduction(){
         <>
         <div className="intro-container">
             <div className="intro-left">
-                <h1>ABOUT SRKR</h1>
-                
-                <img className="founder" src={image3} height="230px" width="160px" />
-            </div>
-            
-            <div className="intro-middle">
-                <div className="intro-mid-left">
-                    <p>Sagi Rama Krishnam Raju Engineering College, established in 1980, is one of the earliest self-financing Engineering Colleges in the state of Andhra Pradesh. Established with a noble cause to empower rural students through technical education, the institution today has evolved as one of the pioneering</p>
+                <div><h1>ABOUT SRKR</h1></div>
+                <div className="block-division">
+                    <div className="intro-left-left">
+                        <img className="founder" src={image3} height="220px" width="160px" />
+                    </div>
+                    <div className="intro-left-right">
+                        <h3>Welcome to SRKREC !</h3>
+                        <p> Sagi Rama Krishnam Raju Engineering College, established in 1980,
+                            is one of the earliest self-financing Engineering Colleges in the state
+                            of Andhra Pradesh. Established with a noble cause to empower rural students
+                            through technical education, the institution today has evolved as one of the
+                            pioneering technical institutions in the country.</p>
+                    </div>
                 </div>
-                <div className="intro-mid-right"></div>
-            </div>
-            <div className="intro-right">
-                <h1>EVENTS</h1>
+                <div>
+                    <p>Spreading over 30 acres of green land, the institution has set in state-of-the-art 
+                        facilities for science and technology and created a conducive environment for 
+                        inclusive and culturally responsive teaching-learning process.Alongside education 
+                        and research, it established a history of work ethos that supports students in 
+                        developing a creative, confident and logical approach to nation building, making 
+                        them highly valued graduates and opening doors to a wide range of exciting careers.</p>    
+                </div>  
             </div>
             
+            <div className="intro-left">
+                <div><h1>ABOUT SRKR</h1></div>
+                <div className="block-division">
+                    <div className="intro-left-left">
+                        <img className="founder" src={image3} height="220px" width="160px" />
+                    </div>
+                    <div className="intro-left-right">
+                        <h3>Welcome to SRKREC !</h3>
+                        <p> Sagi Rama Krishnam Raju Engineering College, established in 1980,
+                            is one of the earliest self-financing Engineering Colleges in the state
+                            of Andhra Pradesh. Established with a noble cause to empower rural students
+                            through technical education, the institution today has evolved as one of the
+                            pioneering technical institutions in the country.</p>
+                    </div>
+                </div>
+                <div>
+                    <p>Spreading over 30 acres of green land, the institution has set in state-of-the-art 
+                        facilities for science and technology and created a conducive environment for 
+                        inclusive and culturally responsive teaching-learning process.Alongside education 
+                        and research, it established a history of work ethos that supports students in 
+                        developing a creative, confident and logical approach to nation building, making 
+                        them highly valued graduates and opening doors to a wide range of exciting careers.</p>    
+                </div>  
+            </div>
+
+
+            <div className="intro-left">
+                <div><h1>ABOUT SRKR</h1></div>
+                <div className="block-division">
+                    <div className="intro-left-left">
+                        <img className="founder" src={image3} height="220px" width="160px" />
+                    </div>
+                    <div className="intro-left-right">
+                        <h3>Welcome to SRKREC !</h3>
+                        <p> Sagi Rama Krishnam Raju Engineering College, established in 1980,
+                            is one of the earliest self-financing Engineering Colleges in the state
+                            of Andhra Pradesh. Established with a noble cause to empower rural students
+                            through technical education, the institution today has evolved as one of the
+                            pioneering technical institutions in the country.</p>
+                    </div>
+                </div>
+                <div>
+                    <p>Spreading over 30 acres of green land, the institution has set in state-of-the-art 
+                        facilities for science and technology and created a conducive environment for 
+                        inclusive and culturally responsive teaching-learning process.Alongside education 
+                        and research, it established a history of work ethos that supports students in 
+                        developing a creative, confident and logical approach to nation building, making 
+                        them highly valued graduates and opening doors to a wide range of exciting careers.</p>    
+                </div>  
+            </div>
+
+
+
         </div>
         </>
     )
