35e06043a3e9d0844b40802b6c29ac291a8e6ca6
Ganesh <masterminds0809@gmail.com>
2023-07-21 23:07:25 +0530

    ok

--- paie_frontend/src/user_components/crousel/crousel.js
+++ paie_frontend/src/user_components/crousel/crousel.js
@@ -42,34 +42,36 @@ function Crousel(){
       };
     return(
         <>
+        <div className="crousel-container">
             <div className='Carousel-c'>
             <Slider {...settings}>
             <div className='box-c'>
               <div className="slider-container">
-                <div id="crousel-image"><img src={image1} height="300px" width="650px" /></div>
+                <div id="crousel-image"><img src={image1} height="300px" width="500px" /></div>
                 <div id="crousel-text"><h2>Mana Ooru Mana Niru </h2></div>
               </div>
             </div>
             <div className='box-c'>
             <div className="slider-container">
-                <div id="crousel-image"><img src={image2} height="300px" width="650px" /></div>
+                <div id="crousel-image"><img src={image2} height="300px" width="500px" /></div>
                 <div id="crousel-text"><h2>INAUGURATION BY PRINCIPAL ON INTERNATIONAL YOGA DAY</h2></div>
               </div>
             </div>
             <div className='box-c'>
             <div className="slider-container">
-                <div id="crousel-image"><img src={image3} height="300px" width="650px" /></div>
+                <div id="crousel-image"><img src={image3} height="300px" width="500px" /></div>
                 <div id="crousel-text"><h2>DRUG FREE INDIA</h2></div>
               </div>
             </div>
             <div className='box-c'>
             <div className="slider-container">
-                <div id="crousel-image"><img src={image1} height="300px" width="650px" /></div>
+                <div id="crousel-image"><img src={image1} height="300px" width="500px" /></div>
                 <div id="crousel-text"><h2></h2></div>
               </div>
             </div>
 
             </Slider>
+            </div>
             </div>
         </>
     )
